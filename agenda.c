0791231ccbd236158b0dfbbf3a6242d243ae9913
A.Leall <83127864+oLeall99@users.noreply.github.com>
2024-04-19 22:52:27 +0000

    Aplicado formatação e validação do número de telefone

--- agenda.c
+++ agenda.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "agenda.h"
 
 // -------------------------------------------------------------------------- |
@@ -8,7 +9,6 @@ Value criar(Contato contatos[], int *pos){
     if( *pos >= TOTAL)
         return MAX_CONTATO;
     
-    char num[T_TELEFONE];
 
     printf("| > Nome do Contato: ");
     fgets(contatos[*pos].nome, T_NOME, stdin);
@@ -16,8 +16,38 @@ Value criar(Contato contatos[], int *pos){
     printf("| > Sobrenome do Contato: ");
     fgets(contatos[*pos].sobrenome, T_SOBRENOME, stdin);
 
-    printf("| > Telefone do Contato (Ex: 01234567890): ");
-    fgets(contatos[*pos].telefone, T_TELEFONE, stdin);
+    char num[T_TELEFONE];
+    int numCorrect;
+    do{
+        numCorrect = 1;
+        printf("| > Telefone do Contato (Ex: 01234567890): ");
+        fgets(num, T_TELEFONE, stdin);
+
+        num[strcspn(num, "\n")] = '\0'; // Remove o \n do final do número informado;
+        if(strlen(num) > 12 || strlen(num) < 10){ // Verifica se o tamanho do número é válido
+            numCorrect = 0;
+        }
+        if(numCorrect){
+            for (int i = 0; num[i] != '\0'; i++) { // Verifica se não há nenhum char não numérico no número informado;
+                if (!isdigit(num[i]) && num[i] != '\0') {
+                    numCorrect = 0;
+                    break;
+                }
+            }
+        }
+
+        if(numCorrect){ // Se passou nas validações anteriores faz a formatação do número
+            formatNumber(num);
+        }
+        else{
+            printf("| > Telefone Inválido, tente novamente...\n");
+            num[0] = '\0';
+        }
+    }while(!numCorrect);
+
+    strcpy(contatos[*pos].telefone, num);
+    // printf("\n| > %s", contatos[*pos].telefone);
+
 
     printf("| > Email do Contato: ");
     fgets(contatos[*pos].email, T_EMAIL, stdin);
@@ -46,5 +76,24 @@ Value carregar(Contato tarefas[], int *pos){
 void clearBuffer(){
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void formatNumber(char telefone[]) {
+
+    // Verifica o tamanho do número de telefone
+    int tamanho = strlen(telefone);
+    if (tamanho == 10) {
+        // Formata como telefone: 11 1234-1234
+        sprintf(telefone, "%c%c %c%c%c%c-%c%c%c%c",
+            telefone[0], telefone[1],
+            telefone[2], telefone[3], telefone[4], telefone[5],
+            telefone[6], telefone[7], telefone[8], telefone[9]);
+    } else if (tamanho == 11) {
+        // Formata como celular: 11 91234-1234
+        sprintf(telefone, "%c%c %c%c%c%c%c-%c%c%c%c",
+            telefone[0], telefone[1],
+            telefone[2], telefone[3], telefone[4], telefone[5], telefone[6],
+            telefone[7], telefone[8], telefone[9], telefone[10]);
+    }
 }
 // -------------------------------------------------------------------------- |
